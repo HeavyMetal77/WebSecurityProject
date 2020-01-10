4667b69d6340a31d7629f4ec124504b277f8bc1b
HeavyMetal77 <ttm2006@ukr.net>
2020-01-10 18:34:36 +0200

    Add JDBC Database Authentication

--- src/main/java/ua/tarastom/springsecurity/config/AppConfig.java
+++ src/main/java/ua/tarastom/springsecurity/config/AppConfig.java
@@ -1,16 +1,61 @@
 package ua.tarastom.springsecurity.config;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.util.logging.Logger;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages="ua.tarastom.springsecurity")
+@PropertySource("classpath:persistence-mysql.properties")
 public class AppConfig {
+
+    private Logger logger = Logger.getLogger(getClass().getName());
+
+    @Autowired
+    private Environment environment; //хранит данные с проперти файла
+
+    @Bean
+    public DataSource securityDataSource() {
+        // create connection pool from c3p0 lib
+        ComboPooledDataSource securityDataSource = new ComboPooledDataSource();
+
+        // set the jdbc driver class
+        try {
+            securityDataSource.setDriverClass(environment.getProperty("jdbc.driver"));
+        } catch (PropertyVetoException e) {
+            throw new RuntimeException(e);
+        }
+
+        // log the connection props
+        // just to make sure we are REALLY reading data from properties file
+        logger.info("\n\n>>> jdbc.url=" + environment.getProperty("jdbc.url"));
+        logger.info("\n\n>>> jdbc.user=" + environment.getProperty("jdbc.user"));
+
+        // set database connection props
+        securityDataSource.setJdbcUrl(environment.getProperty("jdbc.url"));
+        securityDataSource.setUser(environment.getProperty("jdbc.user"));
+        securityDataSource.setPassword(environment.getProperty("jdbc.password"));
+
+        // set connection pool props
+        securityDataSource.setInitialPoolSize(Integer.parseInt(environment.getProperty("connection.pool.initialPoolSize")));
+        securityDataSource.setMinPoolSize(Integer.parseInt(environment.getProperty("connection.pool.minPoolSize")));
+        securityDataSource.setMaxPoolSize(Integer.parseInt(environment.getProperty("connection.pool.maxPoolSize")));
+        securityDataSource.setMaxIdleTime(Integer.parseInt(environment.getProperty("connection.pool.maxIdleTime")));
+
+        return securityDataSource;
+    }
 
     @Bean
     public ViewResolver viewResolver() {
--- src/main/java/ua/tarastom/springsecurity/config/AppSecurityConfig.java
+++ src/main/java/ua/tarastom/springsecurity/config/AppSecurityConfig.java
@@ -1,13 +1,17 @@
 package ua.tarastom.springsecurity.config;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
 
 //import org.springframework.security.core.userdetails.User;
 //import org.springframework.security.core.userdetails.User.UserBuilder;
@@ -16,7 +20,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    //with deprecated method
+    //with deprecated method - при настройке вручную пароли захаркоджены в коде вариант 1 deprecated
     //https://stackoverflow.com/questions/49847791/java-spring-security-user-withdefaultpasswordencoder-is-deprecated
 //    @Override
 //    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -29,8 +33,12 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 //                .withUser(users.username("susan").password("test123").roles("MANAGER", "ADMIN"));
 //    }
 
+    @Autowired
+    private DataSource securityDataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        /* при настройке вручную пароли захаркоджены в коде вариант 2
         auth.inMemoryAuthentication()
                 .passwordEncoder(passwordEncoder())
                 .withUser("john")
@@ -46,13 +54,17 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser("susan")
                 .password(passwordEncoder().encode("test123"))
                 .roles("MANAGER", "ADMIN");
+        */
+
+        auth.jdbcAuthentication().dataSource(securityDataSource); //для чтения юзеров с базы данных
 
     }
 
+    /* при настройке вручную пароли захаркоджены в коде вариант 2
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
+    }*/
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
