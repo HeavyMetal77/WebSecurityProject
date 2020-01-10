2076d57dac43afd6625a49ed823ef0ea10ddf61c
HeavyMetal77 <ttm2006@ukr.net>
2020-01-10 11:11:36 +0200

    замена устаревшего User.withDefaultPasswordEncoder() (deprecated)

--- src/main/java/ua/tarastom/springsecurity/config/AppSecurityConfig.java
+++ src/main/java/ua/tarastom/springsecurity/config/AppSecurityConfig.java
@@ -1,26 +1,57 @@
 package ua.tarastom.springsecurity.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.User.UserBuilder;
 
 @Configuration
 @EnableWebSecurity
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    //with deprecated method
+    //https://stackoverflow.com/questions/49847791/java-spring-security-user-withdefaultpasswordencoder-is-deprecated
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        // add our users for in memory authentication
+//        UserBuilder users = User.withDefaultPasswordEncoder(); //deprecated
+//
+//        auth.inMemoryAuthentication()
+//                .withUser(users.username("john").password("test123").roles("EMPLOYEE"))
+//                .withUser(users.username("mary").password("test123").roles("EMPLOYEE", "MANAGER"))
+//                .withUser(users.username("susan").password("test123").roles("MANAGER", "ADMIN"));
+//    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        // add our users for in memory authentication
-        UserBuilder users = User.withDefaultPasswordEncoder();
-
         auth.inMemoryAuthentication()
-                .withUser(users.username("john").password("test123").roles("EMPLOYEE"))
-                .withUser(users.username("mary").password("test123").roles("EMPLOYEE", "MANAGER"))
-                .withUser(users.username("susan").password("test123").roles("MANAGER", "ADMIN"));
+                .passwordEncoder(passwordEncoder())
+                .withUser("john")
+                .password(passwordEncoder().encode("test123"))
+                .roles("EMPLOYEE");
+        auth.inMemoryAuthentication()
+                .passwordEncoder(passwordEncoder())
+                .withUser("mary")
+                .password(passwordEncoder().encode("test123"))
+                .roles("EMPLOYEE", "MANAGER");
+        auth.inMemoryAuthentication()
+                .passwordEncoder(passwordEncoder())
+                .withUser("susan")
+                .password(passwordEncoder().encode("test123"))
+                .roles("MANAGER", "ADMIN");
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Override
